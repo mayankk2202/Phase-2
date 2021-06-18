64b489865c0e1f2afe451789930cda0c8a0ed50b
Mayank Khatavkar <Mayank.Khatavkar@hotmail.com>
2021-06-18 17:25:27 +0530

    Sprint 1

--- EclipseWorkplace/Phase1Project/src/LockedMeMain.java
+++ EclipseWorkplace/Phase1Project/src/LockedMeMain.java
@@ -1,20 +1,41 @@
+import java.io.*;
 import java.util.LinkedList;
 
 public class LockedMeMain {
 	
 	public static void main(String[] args) {
-//		welcomeScreen obj1 = new welcomeScreen();
+		welcomeScreen obj1 = new welcomeScreen();
 		LinkedList<String> ll1 = new LinkedList<String>();
-		ReadFileInputStream ref1 = new ReadFileInputStream(ll1);
-		ref1.llReady();
+		try{
+			FileReader fr=new FileReader("FileIO");
+			BufferedReader br=new BufferedReader(fr);
+//			StringBuffer sb=new StringBuffer(); 
+			String line;
+			while((line=br.readLine())!=null){
+				ll1.add(line);
+//				sb.append(line); 
+//				sb.append("\n");
+				}
+			fr.close();
+//			System.out.println(sb.toString());
+			}catch(IOException e){
+				e.printStackTrace();
+				}
+	
+		int j = obj1.mainmenu();
 		
-//		int j = obj1.mainmenu();
-		
-//		while (j!=3) {
+		while (j!=3) {
 //			System.out.println("exit not selected.");
-//			j = obj1.mainmenu();
+		if	(j==1) {
+			System.out.println("List of all files:");
+		}else if (j==2) {
+			System.out.println("File Operations");
+		}
+	
+			j = obj1.mainmenu();
 			
-//		}
+		}  
 	}
-
 }
+
+
--- EclipseWorkplace/Phase1Project/src/ReadFileInputStream.java
+++ EclipseWorkplace/Phase1Project/src/ReadFileInputStream.java
@@ -3,13 +3,22 @@ import java.io.IOException;
 import java.util.LinkedList;
 
 public class ReadFileInputStream {
+	
+	LinkedList<String> lili;
+	
 		public ReadFileInputStream(LinkedList<String> ll1) {
 			FileInputStream stream = null;
+			lili = new LinkedList<String>(ll1);
+			char [] filename = new char [256];
 			
 			try {
 				stream = new FileInputStream("fileIO");
 				int data;
 				while((data = stream.read()) != -1) {
+					for	(int ind1=0; data != 13; ind1++ ) {
+						filename[ind1]=(char)data;
+					}
+					
 					System.out.println(data);
 					System.out.println((char)data);
 					System.out.println(data);
@@ -29,6 +38,12 @@ public class ReadFileInputStream {
 		
 		public void llReady() {
 			System.out.println("Linked list ready to be used.");
+
+		}
+		
+		public void printll() {
+			String [] topr = lili.toArray(new String[lili.size()]);
+			System.out.println(topr);
 
 		}
 
