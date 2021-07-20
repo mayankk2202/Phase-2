b0b08859630d3b796ed497367c29fc494806f7a9
Mayank Khatavkar <Mayank.Khatavkar@hotmail.com>
2021-07-20 15:57:04 +0530

    commit to Practice

--- ServletFilterDemo1/src/main/java/com/ServletFilterDemo/LoginFilter.java
+++ ServletFilterDemo1/src/main/java/com/ServletFilterDemo/LoginFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebFilter;
 
 @WebFilter("/LoginFilter")
@@ -21,12 +22,13 @@ public class LoginFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		String Userid = request.getParameter("userid");
 		
+		HttpServletResponse res = (HttpServletResponse) response; 
 		if(Userid != null)
 			chain.doFilter(request, response);
+		else  res.sendRedirect("index.html");
 	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
 		System.out.println("Done!!!");
 	}
-
 }
