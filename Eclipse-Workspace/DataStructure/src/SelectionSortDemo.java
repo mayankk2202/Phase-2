16bb62ead32b0f5063d8d96b6dd9afdd3e738165
mayan <mayan@DESKTOP-MUC6GBJ>
2021-07-24 11:37:29 +0530

    backup

---
+++ Eclipse-Workspace/DataStructure/src/SelectionSortDemo.java
@@ -0,0 +1,34 @@
+
+
+public class SelectionSortDemo {
+	public static void selectionSort(int[] arr){  
+        for (int i = 0; i < arr.length - 1; i++)  
+        {  
+            int index = i;  
+            for (int j = i + 1; j < arr.length; j++){  
+                if (arr[j] < arr[index]){  
+                    index = j;//searching for lowest index  
+                }  
+            }  
+            int smallerNumber = arr[index];   
+            arr[index] = arr[i];  
+            arr[i] = smallerNumber;  
+        }  
+    }  
+       
+    public static void main(String a[]){  
+        int[] arr1 = {12, 34, 56, 23, 89, 4, 0, 2, 76, 89, 21};  
+        System.out.println("Before Selection Sort");  
+        for(int i:arr1){  
+            System.out.print(i+" ");  
+        }  
+        System.out.println();  
+          
+        selectionSort(arr1);//sorting array using selection sort  
+         
+        System.out.println("After Selection Sort");  
+        for(int i:arr1){  
+        	System.out.print(i+" ");  
+        }  
+    }  
+}  
