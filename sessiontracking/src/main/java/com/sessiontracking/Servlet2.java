da8edd78613cf4cbd7bae3724e5affa8526a105d
mayan <mayan@DESKTOP-MUC6GBJ>
2021-07-29 20:24:26 +0530

    Session Tracking Demo - Servlet 2 java file name correction

--- sessiontracking/src/main/java/com/sessiontracking/Servlet2.java
+++ sessiontracking/src/main/java/com/sessiontracking/Servlet2.java
@@ -12,8 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
-@WebServlet("/Servler2")
-public class Servler2 extends HttpServlet {
+@WebServlet("/Servlet2")
+public class Servlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
